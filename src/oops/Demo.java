73acf0b7191a43ad2b9f957fffb6ea4902d5fb29
anujshukla123 <40274446+anujshukla123@users.noreply.github.com>
2025-06-08 22:44:54 +0530

    About Constructor and constructor overloading this keyword in detail

--- src/oops/Demo.java
+++ src/oops/Demo.java
@@ -3,13 +3,44 @@ package oops;
 public class Demo {
     public static void main(String[] args) {
         Student st1=new Student();
+        st1=new Student();//Non-parameterized constructor
+        Student str2=new Student(123);
+        Student str3=new Student(123,"Anuj","Shukla");//parameterized constructor
         st1.studentAge=25;
         st1.studentName="Rajesh";
-        st1.studentCity="Ghazibad";
+        st1.studentCity="Ghaziabad";
         st1.studentId=16383;
         st1.studentAddress="Vijay Nagar Ghaziabad,UP";
         st1.study();
 
+
         st1.showFullDetails();
+        //this Key word: It refers current invoking object
+        /*
+        * this()--> for calling current class constructor from inside
+        * different constructor of same class.
+        *
+        * Class Test(){
+        *
+        * this.a
+        * }
+        * */
+
+        /*****************Inheritance
+        *It is the mechanism in java by which
+         *  one class is allowed to inherit the features of another
+         * class
+        *
+        *Animal class
+         *
+         * Dog extends
+        *
+         *
+         * class Dog extends Animal{
+         *     Eat();
+         *
+         * }
+        * */
+
     }
 }
--- src/oops/Student.java
+++ src/oops/Student.java
@@ -5,19 +5,40 @@ public class Student {
     //Data: datamembers instance variable
     int studentId;
     int studentAge;
-    String studentName;
+    String studentName = "this is Anuj Shukla";
     String studentCity;
     String studentAddress;
 
-    //Behaviour: member method: methods:function
-    public void study()
-    {
-        System.out.println(studentName+" is studying");
+    public Student() {
+        this(1221, "Mayank", "Noida");
+        System.out.println("Crating object");
+
     }
-    public void showFullDetails()
-    {
-        System.out.println("Student Name: "+studentName+", Address is "+
-                studentAddress+" "+studentCity+".\nID is--> "+studentId+" Age is==>"+studentAge);
+
+    public Student(int in) {
+        System.out.println("this is a parameterized cons... " + in);
+    }
+
+    public Student(int a, String studentName, String studentCity) {
+        System.out.println("A value is- " + a + "\nB value is "
+                + studentName);
+        System.out.println("C value is " + studentCity);
+        this.studentId = a;
+        studentName = studentName;
+        studentCity = studentCity;
+        System.out.println("value " + studentName);
+        System.out.println("this value " + this.studentName);
+
+    }
+
+    //Behaviour: member method: methods:function
+    public void study() {
+        System.out.println(studentName + " is studying");
+    }
+
+    public void showFullDetails() {
+        System.out.println("Student Name: " + studentName + ", Address is " +
+                studentAddress + " " + studentCity + ".\nID is--> " + studentId + " Age is==>" + studentAge);
 
     }
 }
